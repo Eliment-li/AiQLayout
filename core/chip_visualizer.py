68a7c213a29b5281e91e91c4230abc90a7006a06
Eliment-li <felix.litian@foxmail.com>
2025-05-09 10:48:16 +0800

    feat: add process bar

--- core/chip_visualizer.py
+++ core/chip_visualizer.py
@@ -29,7 +29,7 @@ class GridTile(Enum):
 class ChipVisualizer:
 
     # Initialize the grid size. Pass in an integer seed to make randomness (Targets) repeatable.
-    def __init__(self, chip:Chip, fps=1, num_qubits=1):
+    def __init__(self, chip:Chip, fps, total_frames):
         self.chip = chip
         self.grid_rows = self.chip.rows
         self.grid_cols = self.chip.cols
@@ -39,8 +39,8 @@ class ChipVisualizer:
         self.last_action = ''
         self._init_pygame()
 
-        self.num_qubits = num_qubits
-
+        self.num_qubits = chip.num_qubits
+        self.total_frames = total_frames  # 总渲染次数
 
 
     def _init_pygame(self):
@@ -63,15 +63,26 @@ class ChipVisualizer:
 
         # Define game window size (width, height)
         self.window_size = (
-        self.cell_width * self.grid_cols, self.cell_height * self.grid_rows + self.action_info_height)
+            self.cell_width * self.grid_cols, self.cell_height * self.grid_rows + self.action_info_height + 20)
 
         # Initialize game window
         self.window_surface = pygame.display.set_mode(self.window_size)
 
+        # 进度条相关参数
+        self.progress_bar_width = self.window_surface.get_width()  # 进度条宽度
+        self.progress_bar_height = 20  # 进度条高度
+        self.progress_bar_color = (0, 128, 0)  # 进度条颜色
+        self.progress_bar_background_color = (200, 200, 200)  # 进度条背景颜色
+        self.progress_bar_y = self.window_surface.get_height() - self.progress_bar_height  # 进度条的 Y 坐标
+        self.current_frame = 0  # 当前渲染次数
 
+        # 拖动状态
+        self.is_dragging = False
 
         # Load & resize objects(qubits, magic state, broken qubits, etc.)
         self.qubit_imgs = self.get_qubits_object()
+
+
 
 
         file_name =  img_path / 'tile_light_grey.png'
@@ -83,6 +94,19 @@ class ChipVisualizer:
         img = pygame.image.load(file_name)
         self.goal_img = pygame.transform.scale(img, self.cell_size)
         self.goal_img = self.add_text_to_img(self.goal_img,1, )
+
+    def _draw_progress_bar(self):
+        # 计算进度条的当前宽度
+        progress_ratio = self.current_frame / self.total_frames
+        current_width = int(self.progress_bar_width * progress_ratio)
+
+        # 绘制进度条背景
+        progress_bar_rect = pygame.Rect(0, self.progress_bar_y, self.progress_bar_width, self.progress_bar_height)
+        pygame.draw.rect(self.window_surface, self.progress_bar_background_color, progress_bar_rect)
+
+        # 绘制当前进度
+        current_progress_rect = pygame.Rect(0, self.progress_bar_y, current_width, self.progress_bar_height)
+        pygame.draw.rect(self.window_surface, self.progress_bar_color, current_progress_rect)
 
     def get_qubits_object(self) -> list:
         qubit_imags = []
@@ -143,18 +167,20 @@ class ChipVisualizer:
                 self.window_surface.blit(self.floor_img, pos)
 
         #render the qubits
-        print('position:', self.chip.position)
+        #print('position:', self.chip.position)
         for i in range(0, self.chip.num_qubits):
             r,c = self.chip.position[i+1]
             pos = (c * self.cell_width, r * self.cell_height)
             self.window_surface.blit(self.qubit_imgs[i], pos)
+
+        self.current_frame += 1
+        self._draw_progress_bar()
         #show bottom text
         # text_img = self.action_font.render(f'Action: {self.last_action}', True, (0, 0, 0), (255, 255, 255))
         # text_pos = (0, self.window_size[1] - self.action_info_height)
         # self.window_surface.blit(text_img, text_pos)
 
         pygame.display.update()
-
 
         # Limit frames per second
         self.clock.tick(self.fps)
@@ -178,9 +204,10 @@ class ChipVisualizer:
 
 def show_trace(trace:list):
     chip = Chip(num_qubits=len(trace),cols=args.chip_cols,rows=args.chip_rows)
-    CV = ChipVisualizer(chip, fps=2)
+    total_frames = len(trace[0])
+    CV = ChipVisualizer(chip = chip, fps=4,total_frames=total_frames)
     CV.render()
-    for j in range(len(trace[0])):
+    for j in range(total_frames):
          for i in range(len(trace)):
             if j >= len(trace[i]):
                 continue
@@ -200,5 +227,6 @@ if __name__ == "__main__":
     #     player = random.randint(1, chip.num_qubits)
     #     warehouseRobot.perform_action(player,rand_action)
     #     warehouseRobot.render()
-    trace = [[3, 3, 3, 3,3,1,3, 3, 3, 3, 3, 3, 2, 2], [3, 3, 3, 3, 3, 3, 3, 3, 0, 2], [3, 3, 1, 1, 1, 1, 1, 1, 1, 1], [3, 1, 3, 3, 1, 3, 1, 3, 0, 0]]
+    trace = [[3, 3, 3, 3, 0, 0, 0, 0, 2, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 1, 3, 0, 0, 3, 0, 3, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1], [3, 1, 1, 1, 3, 2, 1, 0, 3, 3, 3, 2, 1, 3, 0, 1, 3, 1, 0, 1, 0, 1, 0, 1, 0, 1, 3, 2, 0, 1, 3, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1], [1, 1, 3, 1, 1, 0, 3, 1, 2, 1, 3, 0, 2, 2, 3, 0, 1, 1, 0, 0, 1, 3, 2, 1, 3, 1, 1, 2, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0], [2, 1, 1, 3, 1, 1, 0, 1, 2, 1, 2, 3, 3, 0, 3, 1, 2, 0, 2, 0, 2, 0, 2, 2, 0, 3, 1, 2, 2, 0, 1, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3]]
+
     show_trace(trace)
