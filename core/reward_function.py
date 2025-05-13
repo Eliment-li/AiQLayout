980d7046652b59621e4e7b1aadc9ab5da76b788d
Eliment-li <felix.litian@foxmail.com>
2025-05-13 11:30:59 +0800

    sync

--- core/reward_function.py
+++ core/reward_function.py
@@ -5,20 +5,15 @@ import queue
 
 from sympy.abc import alpha
 
+from collections import deque
 
-# 初始化一个队列，最大容量为 5（可选）
+
+
 
 
 class RewardFunction:
     def __init__(self):
-
-        self.window_size = 5
-        self.p_dist = 0
-        self.recent_dist = [0] * self.window_size
-        self.min_dist = np.inf
-        self.max_total_r = -np.inf
-
-    #default
+        pass
     '''
     引导 agent 逐步逼近目标位置来计算reward
     '''
@@ -42,10 +37,13 @@ class RewardFunction:
 
         return r
 
+    def _recent_dist_avg(self):
+        return np.mean(self.recent_dist)
 
-    def rfv2(self,init_dist,last_dist,dist,agent_total_r):
-        k1 = (init_dist - dist) / init_dist
-        k2 = (last_dist - dist) / init_dist
+    def rfv2(self,init_dist,last_dist,avg_dist,dist):
+        #update recent dist
+        k1 = (dist - init_dist) / avg_dist
+        k2 = (dist - last_dist) / avg_dist
 
         # if abs(k1)>=0.5:
         #     k1 = 0.5 if k1 > 0 else -0.5
@@ -68,16 +66,6 @@ class RewardFunction:
         else:
             r = -0.05
 
-        gamma = 0.99
-
-        if dist < self.min_dist:
-            if self.min_dist != np.inf:
-                r =  (self.max_total_r - agent_total_r * gamma)*(1+(self.min_dist-dist)/self.min_dist)*1.1
-            self.min_dist = dist
-
-        if self.max_total_r < r:
-            self.max_total_r = r
-
         return r
 
 
@@ -89,7 +77,7 @@ def test_rf(distance: list):
     last_dist = init_dist
     for i in range(1,len(distance)):
         dist = distance[i]
-        reward = rf.rfv2(init_dist=init_dist,last_dist=last_dist, dist=dist,agent_total_r=total)
+        reward = rf.rfv2(init_dist=init_dist,last_dist=last_dist, dist=dist)
         last_dist = dist
 
         total *= 0.99
@@ -102,7 +90,7 @@ if __name__ == '__main__':
     #dist=[1,2,3,4,5]
 
     #dist=[1,2,1,2,1,2,1,2]
-    dist = [5,10,5,10,5,4, 8,4]
+    dist = [4,10,5,10,5,4, 8,11]
     #dist = [5,5,4,4,5,5,4]
     test_rf(dist)
 
